e716f8096f46dab1cb77560dc744c1360cbeb285
Sai Teja Are <saiteja.are1@gmail.com>
2025-04-08 15:09:40 +0530

    lint changes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+"""This module contains a simple hello_world function to demonstrate CI/CD."""
+
 def hello_world():
     """Prints Hello, World!"""
     print("Hello, World!")
 
 if __name__ == "__main__":
     hello_world()
+
