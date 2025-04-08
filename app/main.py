893e78d3dfb9dbd60e4c042759da11698279b7a4
Sai Teja Are <saiteja.are1@gmail.com>
2025-04-08 15:12:55 +0530

    black changes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 """This module contains a simple hello_world function to demonstrate CI/CD."""
 
+
 def hello_world():
     """Prints Hello, World!"""
     print("Hello, World!")
 
+
 if __name__ == "__main__":
     hello_world()
-
